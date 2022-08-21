e0d3fb5dd90b2e68e0710421db30f5525e9b5512
jayasimhareddy27 <jayasimhareddy27@gmail.com>
2022-08-21 12:07:04 +0530

    finished editing

--- server/controller/postcontroller.js
+++ server/controller/postcontroller.js
@@ -4,10 +4,10 @@ export const postdata=async(req,res)=>{
     const {form,email}=req.body;
     const data=form;
     try {
-        const userdata=await user.findOne({email:email});
-        //console.log(userdata);
+        let userdata=await user.findOne({email:email});
+        let branch=userdata.trees.filter(x => x.id != data.id)
+        userdata.trees=branch
         userdata.trees.push(data);
-        //console.log(userdata);
         const newdata=new user(userdata);
         await newdata.save()
         res.status(200).json(userdata.trees);    
@@ -17,11 +17,21 @@ export const postdata=async(req,res)=>{
 }
 export const getd=async (req,res)=>{
     const {email}=req.body;
-    //console.log(email);
     try {        
         const userdata=await user.findOne({email:email});
-        //console.log(userdata);
         await res.status(200).json(userdata.trees);  
+    }
+    catch (error) {
+            res.status(404).json({message:error.message});
+    }
+}
+
+export const getid=async (req,res)=>{
+    const {id,email}=req.params;
+    try {        
+        const userdata=await user.findOne({email:email});
+        const branch=userdata.trees.filter(data => data.id == id)
+        await res.status(200).json(branch);  
     }
     catch (error) {
             res.status(404).json({message:error.message});
--- server/routes/postrouter.js
+++ server/routes/postrouter.js
@@ -1,5 +1,5 @@
 import Express from 'express';
-import {postdata,del,getd} from './../controller/postcontroller.js'
+import {postdata,del,getd,getid} from './../controller/postcontroller.js'
 
 
 const router=Express.Router();
@@ -7,5 +7,6 @@ const router=Express.Router();
 router.post('/byemail',getd);
 router.post('/',postdata);
 router.delete('/:a/:b',del);
+router.patch('/byemail/:id/:email',getid);
 
 export default router;
